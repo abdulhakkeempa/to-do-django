f97bb4c2817a6f58c7fed29427c2c97dc85ce3d4
hakkeempa <abdulhakkeempa2002@gmail.com>
2022-06-01 00:06:44 +0530

    url-route-errors via js , need to fix

--- project/urls.py
+++ project/urls.py
@@ -4,7 +4,7 @@ from .import views
 urlpatterns = [
     path('',views.homePage,name='homePage'),
     path('addtasks',views.addProject,name='addtasks'),
-    path('viewtasks',views.displayTask,name='viewtasks'),
+    path('viewtasks/',views.displayTask,name='viewtasks'),
     path('deletetasks/<str:pk>',views.deleteTask,name='deletetasks'),
     path('updatetasks/<str:pk>',views.updateTask,name='updatetasks'),
     path('updatestatus/<str:pk>',views.updateStatus,name='updatestatus'),
