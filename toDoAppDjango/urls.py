e2449ef41a396d1d588d66f2e004c1e4885b5b25
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-01-26 21:29:11 +0530

    created api urls

--- toDoAppDjango/urls.py
+++ toDoAppDjango/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include('project.urls'))
+    path("",include('project.urls')),
+    path("api/",include('api.urls')),
 ]
