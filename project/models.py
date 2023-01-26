7491ca552408b1851119a66d07c3b2da31531477
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-01-26 19:57:10 +0530

    made task model for user based

--- project/models.py
+++ project/models.py
@@ -1,13 +1,16 @@
-from statistics import mode
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class toDoList(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    id = models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
     task = models.CharField(max_length=100,null=False,blank=False)
     remarks = models.TextField(max_length=100,null=True,blank=True)
     status = models.CharField(max_length=100,default='Incomplete')
     timeAdded = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
+
     def __str__(self) :
         return self.task
