5e2125e166de0fa547db45d47e45f3ccea1b789a
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-01-26 21:27:25 +0530

    added drf to settings

--- toDoAppDjango/settings.py
+++ toDoAppDjango/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'project.apps.ProjectConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -112,6 +113,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
