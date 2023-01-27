ae8261c48ac066152cc5352d59efb6f638f975bd
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-01-27 23:23:01 +0530

    jwt tokens working need to implement login api

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,18 @@
 from django.urls import include, path
 from rest_framework import routers
 from .views import ToDoViewSet,UsersViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 router = routers.DefaultRouter()
 router.register(r'tasks', ToDoViewSet)
 router.register(r'users', UsersViewSet)
-
+  
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
