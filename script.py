a566b96c528643f727d7fb1f41f22d2d52d9edbd
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-02-02 22:46:53 +0530

    update script.py

--- script.py
+++ script.py
@@ -1,8 +1,16 @@
 import os
 from django.core.management.utils import get_random_secret_key  
 
-with open('.env', 'w') as fp:
-    #generate secret key automatically
-    key = get_random_secret_key()
-    fp.write('SECRET_KEY="'+key+'"')
-    fp.close()
+def generateEnv():
+    """
+    Function to generate an env variable and append the django secret key inside.
+    """
+    with open('.env', 'w') as fp:
+        #generate secret key automatically
+        key = get_random_secret_key()
+        fp.write('SECRET_KEY="'+key+'"')
+        fp.close()
+
+
+if __name__=="__main__":
+    generateEnv()
