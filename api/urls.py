4d216028048adbe15636aeb50701bbea3a8d3210
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-02-02 22:42:27 +0530

    get object method added but need to check permissions

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import include, path,re_path
 from rest_framework import routers
 from .views import ToDoViewSet,UsersViewSet,UserCreateAPI
 from rest_framework_simplejwt.views import (
@@ -16,5 +16,6 @@ urlpatterns = [
     path('auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/',UserCreateAPI.as_view()),
-    path('tasks/',ToDoViewSet.as_view())
+    path('tasks/',ToDoViewSet.as_view()),
+    path('tasks/<str:pk>',ToDoViewSet.get_object)
 ]
--- api/views.py
+++ api/views.py
@@ -3,13 +3,14 @@ from rest_framework import viewsets,generics
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework.permissions import AllowAny
 from django.db.models import Count
-
+from django.http import JsonResponse
 
 from .serializer import ToDoModelSerializer,UserModelSerializer
 from project.models import toDoList
 from django.contrib.auth.models import User
 
 from rest_framework import generics
+from rest_framework import views
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.response import Response
@@ -19,35 +20,53 @@ from django.contrib.auth import authenticate
 
 # Create your views here.
 
-class ToDoViewSet(generics.GenericAPIView):
+class ToDoViewSet(views.APIView):
   queryset = toDoList.objects.all()
   serializer_class = ToDoModelSerializer
   permission_classes = [IsAuthenticated]
+  lookup_field = 'id'
 
   def get(self, request, *args, **kwargs):
     """
     API End Point to fetch the tasks created by the requested user.
     """
     queryset = toDoList.objects.filter(user=self.request.user)
-    data = self.get_serializer(queryset,many=True)
+    data = ToDoModelSerializer(queryset,many=True)
     return Response(data.data)
 
   def post(self, request, *args, **kwargs):
     """
     API End Point to create a task for a requested user.
     """
-    serializer = self.get_serializer(data=request.data)
+    serializer = ToDoModelSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     task = serializer.save()
     return Response(serializer.data,status=status.HTTP_201_CREATED)
 
   def get_object(self, pk):
+    """
+    API Endpoint for fetching an single object with the primary key passed.
+    """
     try:
-        data = toDoList.objects.get(pk=pk)
-        data = self.get_serializer(data)
-        return Response(data.data)
+      task = toDoList.objects.get(id=pk)
+      serializer = ToDoModelSerializer(task)
     except toDoList.DoesNotExist:
-        raise Response(status=status.HTTP_404_NOT_FOUND)
+      message = {
+        "message":"Data not found"
+      }
+      raise Response(message,status=status.HTTP_404_NOT_FOUND)
+    
+    #self.user is logged user not jwt user
+    print(self.user.id)
+    print(task.user.id)
+
+    if task.user.id == self.user.id:
+      return JsonResponse(serializer.data,status=status.HTTP_200_OK)
+    else:
+      message = {
+        "message":"Forbidden"
+      }
+      return JsonResponse(message,status=status.HTTP_403_FORBIDDEN)
 
 
 class UsersViewSet(viewsets.ModelViewSet):
